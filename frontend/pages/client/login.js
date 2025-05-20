7a76e251b7cf3a4466c2cbbdf9c5274e49897408
MadheshKumarVJ <vjmadheshkumarofficial@gmail.com>
2025-05-20 19:44:25 +0530

    add images

--- frontend/pages/client/login.js
+++ frontend/pages/client/login.js
@@ -193,7 +193,7 @@ export default function Login() {
         <div className="relative hidden w-0 flex-1 lg:block">
           <img
             alt=""
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            src="./undraw_business-decisions.svg"
             className="absolute inset-0 size-full object-cover"
           />
         </div>
