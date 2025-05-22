2a86940e70f1545e2e1ee4e7c65dc93df5375f6e
MadheshKumarVJ <112080679+MadheshKumarVJ@users.noreply.github.com>
2025-05-23 03:26:28 +0530

    feat: Integrate reset password pages (#8)
    ---------
    
    Co-authored-by: Deepaanand2003 <deepaanandm2003@gmail.com>

---
+++ frontend/pages/login/resetpassword.js
@@ -0,0 +1,197 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function ResetPasswordConfirm() {
+    const router = useRouter();
+    const { uid, token } = router.query;
+
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    useEffect(() => {
+        setError('');
+        setSuccess('');
+    }, [uid, token]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (newPassword !== confirmPassword) {
+            setError('Passwords do not match.');
+            return;
+        }
+
+        try {
+            await axios.post('http://127.0.0.1:8000/api/v1/auth/password-reset-confirm/', {
+                uid,
+                token,
+                new_password: newPassword,
+            });
+
+            setSuccess('Password reset successful! Redirecting to login...');
+            setTimeout(() => {
+                router.push('/login');
+            }, 2000);
+        } catch (err) {
+            console.error(err);
+            setError(err.response?.data?.detail || 'Something went wrong.');
+        }
+    };
+
+    return (
+        <div className="w-[500px] relative left-[500px] top-[125px]">
+            <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="p-4 sm:p-7">
+                    <div className="text-center">
+                        <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Enter your New Password</h1>
+
+                    </div>
+
+                    <div className="mt-5">
+
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                        {success && <p style={{ color: 'green' }}>{success}</p>}
+                        <form onSubmit={handleSubmit}>
+                            <div className="grid gap-y-4">
+
+                                <div>
+                                    <label htmlFor="password" className="block text-sm mb-2 dark:text-white">
+                                        New Password
+                                    </label>
+                                    <div className="relative">
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            placeholder="New password"
+                                            value={newPassword}
+                                            onChange={(e) => setNewPassword(e.target.value)}
+
+                                            name="password"
+                                            className="border py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                            required
+                                            aria-describedby="password-error"
+                                        />
+                                        <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                            <svg
+                                                className="size-5 text-red-500"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                viewBox="0 0 16 16"
+                                                aria-hidden="true"
+                                            >
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <p className="hidden text-xs text-red-600 mt-2" id="password-error">
+                                        8+ characters required
+                                    </p>
+                                </div>
+
+                                {/* Confirm Password */}
+                                <div>
+                                    <label htmlFor="confirm-password" className="block text-sm mb-2 dark:text-white">
+                                        Confirm Password
+                                    </label>
+                                    <div className="relative">
+                                        <input
+                                            type="password"
+                                            placeholder="Confirm new password"
+                                            value={confirmPassword}
+                                            onChange={(e) => setConfirmPassword(e.target.value)}
+                                            id="confirm-password"
+                                            name="confirm-password"
+                                            className="border py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                            required
+                                            aria-describedby="confirm-password-error"
+                                        />
+                                        <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                            <svg
+                                                className="size-5 text-red-500"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                viewBox="0 0 16 16"
+                                                aria-hidden="true"
+                                            >
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <p className="hidden text-xs text-red-600 mt-2" id="confirm-password-error">
+                                        Password does not match the password
+                                    </p>
+                                </div>
+
+                                {/* Terms Checkbox */}
+                                <div className="flex items-center">
+                                    <div className="flex">
+                                        <input
+                                            id="remember-me"
+                                            name="remember-me"
+                                            type="checkbox"
+                                            className="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                                        />
+                                    </div>
+                                    <div className="ms-3">
+                                        {/* <label
+                                            htmlFor="remember-me"
+                                            className="text-sm dark:text-white"
+                                        >
+                                            I accept the{' '}
+                                            <a
+                                                href="#"
+                                                className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500"
+                                            >
+                                                Terms and Conditions
+                                            </a>
+                                        </label> */}
+                                    </div>
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                                >
+                                    Sign up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        //  <div style={{ padding: '2rem' }}>
+        //   <h2>Reset Password</h2>
+        //   {error && <p style={{ color: 'red' }}>{error}</p>}
+        //   {success && <p style={{ color: 'green' }}>{success}</p>}
+        //   <form onSubmit={handleSubmit}>
+        //     <input
+        //       type="password"
+        //       placeholder="New password"
+        //       value={newPassword}
+        //       onChange={(e) => setNewPassword(e.target.value)}
+        //       required
+        //     />
+        //     <br />
+        //     <input
+        //       type="password"
+        //       placeholder="Confirm new password"
+        //       value={confirmPassword}
+        //       onChange={(e) => setConfirmPassword(e.target.value)}
+        //       required
+        //     />
+        //     <br />
+        //     <button type="submit">Reset Password</button>
+        //   </form>
+        // </div>
+    );
+}
+
