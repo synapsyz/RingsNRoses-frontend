d16d803934dde3a96d4c89158f3a17fb90946f95
Deepaanand2003 <deepaanandm2003@gmail.com>
2025-06-24 12:57:05 +0530

    remove svg

--- frontend/components/vendor/OrdersChart.js
+++ frontend/components/vendor/OrdersChart.js
@@ -210,9 +210,6 @@ const OrdersChart = () => {
         ))}
       </select>
       <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-        <svg className="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="m6 9 6 6 6-6"/>
-        </svg>
       </div>
     </div>
   );
