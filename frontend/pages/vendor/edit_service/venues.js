492a1f769e94d32c94b44973c5a9c46c75de5d70
Deepaanand2003 <deepaanandm2003@gmail.com>
2025-06-24 11:43:29 +0530

    vendorpage

--- frontend/pages/vendor/edit_service/venues.js
+++ frontend/pages/vendor/edit_service/venues.js
@@ -876,7 +876,7 @@ try {
                         </div>
                         <div>
                           <label htmlFor="managerName" className="block mb-2 text-sm font-medium text-stone-800 dark:text-neutral-200">
-                            Manager Name
+                            Contact Person
                           </label>
                           <input
                             id="managerName"
@@ -958,7 +958,7 @@ try {
                   <div className="flex flex-col bg-white border border-stone-200 overflow-hidden rounded-xl shadow-2xs dark:bg-neutral-800 dark:border-neutral-700">
                     <div className="py-3 px-5 flex justify-between items-center gap-x-5 border-b border-stone-200 dark:border-neutral-700">
                       <h2 className="inline-block font-semibold text-stone-800 dark:text-neutral-200">
-                        Services Offered
+                        Facilities
                       </h2>
                     </div>
                     <div className="p-4">
